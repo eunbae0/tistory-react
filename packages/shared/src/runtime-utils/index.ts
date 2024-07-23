32cfc05e000715dfc76124b44dac14da3cd11741
EunBae Gong <geb9211@gmail.com>
2024-07-23 21:14:19 +0900

    feat: add runtime utility function

--- packages/shared/src/runtime-utils/index.ts
+++ packages/shared/src/runtime-utils/index.ts
@@ -1,1 +1,11 @@
-export const isProduction = () => process.env.NODE_ENV === 'production';
+export const TISTORY_REACT_TEMP_DIR = ".tistory-react";
+
+export const isProduction = () => process.env.NODE_ENV === "production";
+
+export function removeLeadingSlash(url: string) {
+	return url.charAt(0) === "/" ? url.slice(1) : url;
+}
+
+export function removeTrailingSlash(url: string) {
+	return url.charAt(url.length - 1) === "/" ? url.slice(0, -1) : url;
+}
