bde06c38992f7b7bf126047bca6b7a75cf796f21
EunBae Gong <geb9211@gmail.com>
2024-08-10 22:41:33 +0900

    feat: add global virtual-routes type

---
+++ packages/runtime/src/global.d.ts
@@ -0,0 +1,11 @@
+declare module 'virtual-routes' {
+  import { Route } from '@tistory-react/shared';
+
+  export const routes: Route[];
+}
+
+declare module 'virtual-routes-ssr' {
+  import { Route } from '@tistory-react/shared';
+
+  export const routes: Route[];
+}
