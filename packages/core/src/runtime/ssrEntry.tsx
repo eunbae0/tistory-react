6b2aca96085c66e02e246afb16bb60c6cdf7fb3b
EunBae Gong <geb9211@gmail.com>
2024-08-10 22:41:03 +0900

    feat: remove pagePath props in render function

--- packages/core/src/runtime/ssrEntry.tsx
+++ packages/core/src/runtime/ssrEntry.tsx
@@ -2,9 +2,9 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter } from '@tistory-react/runtime/server';
 import { App } from './App';
 
-export async function render(pagePath: string): Promise<{ appHtml: string }> {
+export async function render(): Promise<{ appHtml: string }> {
   const appHtml = renderToString(
-    <StaticRouter location={pagePath}>
+    <StaticRouter>
       <App />
     </StaticRouter>,
   );
