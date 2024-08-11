d29998235a7acea1ab58f74a53b27026f050140a
EunBae Gong <geb9211@gmail.com>
2024-08-11 10:03:56 +0900

    feat: update template layout file

--- packages/create-tistory-react/templates/ts/src/Layout.tsx
+++ packages/create-tistory-react/templates/ts/src/Layout.tsx
@@ -1,3 +1,12 @@
-export default function Layout() {
-  return <div>Layout</div>;
+import type { PropsWithChildren } from 'react';
+
+export type LayoutComponentProps = PropsWithChildren;
+
+export default function Layout({ children }: LayoutComponentProps) {
+  return (
+    <div>
+      Layout
+      {children}
+    </div>
+  );
 }
