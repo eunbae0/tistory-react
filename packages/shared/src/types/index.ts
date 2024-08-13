4268656a20cf1ff2320b3ca0d02a6a0937a5b60b
EunBae Gong <geb9211@gmail.com>
2024-08-13 23:34:44 +0900

    feat: change UserConfig options list

--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -17,26 +17,6 @@ export interface ReplaceRule {
   replace: string;
 }
 
-export interface RouteOptions {
-  /**
-   * The extension name of the filepath that will be converted to a route
-   * @default ['js','jsx','ts','tsx','md','mdx']
-   */
-  extensions?: string[];
-  /**
-   * Include extra files from being converted to routes
-   */
-  include?: string[];
-  /**
-   * Exclude files from being converted to routes
-   */
-  exclude?: string[];
-  /**
-   * use links without .html files
-   */
-  cleanUrls?: boolean;
-}
-
 export interface UserConfig {
   /**
    * The root directory of the site.
@@ -59,10 +39,6 @@ export interface UserConfig {
    */
   icon?: string;
   /**
-   * Language of the site.
-   */
-  lang?: string;
-  /**
    * Title of the site.
    */
   title?: string;
@@ -80,86 +56,16 @@ export interface UserConfig {
         route: RouteMeta,
       ) => string | [string, Record<string, string>] | undefined)
   )[];
-  // /**
-  //  * I18n config of the site.
-  //  */
-  // locales?: Locale[];
-  // /**
-  //  * The i18n text data source path. Default is `i18n.json` in cwd.
-  //  */
-  // i18nSourcePath?: string;
-  // /**
-  //  * Theme config.
-  //  */
-  // themeConfig?: ThemeConfig;
   /**
    * Rsbuild Configuration
    */
   builderConfig?: RsbuildConfig;
   /**
-   * The custom config of vite-plugin-route
-   */
-  route?: RouteOptions;
-  // /**
-  //  * The custom config of markdown compile
-  //  */
-  // markdown?: MarkdownOptions;
-  // /**
-  //  * Doc plugins
-  //  */
-  // plugins?: RspressPlugin[];
-  /**
-   * Replace rule, will replace the content of the page.
-   */
-  replaceRules?: ReplaceRule[];
-  /**
    * Output directory
    */
   outDir?: string;
   /**
-   * Custom theme directory
-   */
-  themeDir?: string;
-  /**
-   * Global components
-   */
-  globalUIComponents?: (string | [string, object])[];
-  /**
-   * Global styles, is a Absolute path
-   */
-  globalStyles?: string;
-  // /**
-  //  * Search options
-  //  */
-  // search?: SearchOptions;
-  /**
-   * Whether to enable ssg, default is true
-   */
-  ssg?: boolean;
-  // /**
-  //  * Whether to enable medium-zoom, default is true
-  //  */
-  // mediumZoom?:
-  //   | boolean
-  //   | {
-  //       selector?: string;
-  //       options?: ZoomOptions;
-  //     };
-  /**
    * Add some extra builder plugins
    */
   builderPlugins?: RsbuildPlugin[];
-  /**
-   * Multi version config
-   */
-  multiVersion?: {
-    /**
-     * The default version
-     */
-    default?: string;
-    /**
-     * The version list, such as ['v1', 'v2']
-     */
-    versions: string[];
-  };
 }
