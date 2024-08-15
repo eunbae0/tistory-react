c901922c8a8a2f06a6da92cd72006bde1fee97a4
EunBae Gong <geb9211@gmail.com>
2024-08-16 03:50:31 +0900

    feat: add create-tistory-react template files

---
+++ packages/create-tistory-react/template/src/Layout.tsx
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from 'react';
+
+export type LayoutComponentProps = PropsWithChildren;
+
+export default function Layout({ children }: LayoutComponentProps) {
+  return <>{children}</>;
+}
---
+++ packages/create-tistory-react/template/tistory-react.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tistory-react/config';
+
+export default defineConfig({
+  root: '.',
+  skinInfoConfig: {
+    default: {
+      showListLock: 1,
+    },
+  },
+});
