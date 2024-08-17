dbc9fc53946d908a964f8b28a97a6584694eba7e
EunBae Gong <geb9211@gmail.com>
2024-08-18 03:33:07 +0900

    feat: add Tag component

---
+++ packages/theme/src/components/Tags/index.tsx
@@ -0,0 +1,51 @@
+import type {
+  PropsWithChildren,
+  RepAnchorProps,
+  RepWrapperProps,
+} from '#theme/types';
+
+/**
+ * 태그 클라우드는 사용한 태그 리스트를 출력합니다.
+ * pages/tags에서 사용합니다.
+
+ * @example 
+  ```
+    <Tags>
+      <h2>태그 클라우드</h2>
+      <ul>
+        <Tags.Rep>
+          <li>
+            <Tags.Anchor className="custom_style" />
+          </li>
+        </Tags.Rep>
+      </ul>
+    </Tags>
+  ```
+ */
+export const Tags = (props: RepWrapperProps) => {
+  return (
+    <s_tag>
+      <div {...props} />
+    </s_tag>
+  );
+};
+
+const Rep = ({ children }: PropsWithChildren) => {
+  return <s_tag_rep>{children}</s_tag_rep>;
+};
+
+const Anchor = (props: RepAnchorProps) => {
+  const { className, ...rest } = props;
+  return (
+    <a
+      href="[##_tag_link_##]"
+      className={`[##_tag_class_##], ${className}`}
+      {...rest}
+    >
+      [##_tag_name_##]
+    </a>
+  );
+};
+
+Tags.Rep = Rep;
+Tags.Anchor = Anchor;
