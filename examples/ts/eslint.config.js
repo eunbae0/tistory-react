71ad70a62724cc60b2c438b3240a4bb5af100a10
EunBae Gong <geb9211@gmail.com>
2024-10-15 21:02:57 +0900

    chore: eslint-plugin-tistory-react environment setting to examples/ts

--- examples/ts/eslint.config.js
+++ examples/ts/eslint.config.js
@@ -1,21 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
+import tistoryReact from 'eslint-plugin-tistory-react';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], rules: {
-    "react/react-in-jsx-scope": "off"
-  }},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['./src/**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  tistoryReact.configs.flat.recommended,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      // 'tistory/tistory-react-components': 'warn',
     },
-    plugins: ['@tistory-react/eslint']
-  }
+  },
 ];
