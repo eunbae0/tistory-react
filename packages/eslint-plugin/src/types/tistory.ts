ba43bf01fe2e91046c66ba977f39fd57f10a530d
EunBae Gong <geb9211@gmail.com>
2024-10-16 22:57:34 +0900

    feat: remove not used type

--- packages/eslint-plugin/src/types/tistory.ts
+++ packages/eslint-plugin/src/types/tistory.ts
@@ -1,14 +1,3 @@
-import type { Article } from '@tistory-react/theme-default/Article';
-import type { Comment } from '@tistory-react/theme-default/Comment';
-import type { Sidebar } from '@tistory-react/theme-default/Sidebar';
-import type { Tags } from '@tistory-react/theme-default/Tags';
-
-export type ComponentsType =
-  | typeof Article
-  | typeof Comment
-  | typeof Sidebar
-  | typeof Tags;
-
 export type TistoryComponentsEnum =
   | 'Article'
   | 'Comment'
