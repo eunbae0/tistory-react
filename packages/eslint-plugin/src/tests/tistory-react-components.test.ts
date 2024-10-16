1c323ae1d77e0617474efeadb2537dad0b79c838
EunBae Gong <geb9211@gmail.com>
2024-10-16 23:02:14 +0900

    feat: add case-specific test

--- packages/eslint-plugin/src/tests/tistory-react-components.test.ts
+++ packages/eslint-plugin/src/tests/tistory-react-components.test.ts
@@ -12,59 +12,165 @@ const ruleTester = new RuleTester({
 ruleTester.run('tistory-react-components', tistoryReactComponentsRule, {
   valid: [
     {
-      code: `import {Article} from 'tistory-react/theme'
+      code: `import {Article, ARTICLE_COMMENT_COUNT} from 'tistory-react/theme/Article'
+      import {Comment} from 'tistory-react/theme/Comment'
           const Components = () => {
             return (
               <Article>
                 <div>
-                <Article.Thumbnail>
-                {ARTICLE_THUMBNAIL_IMG}
-                </Article.Thumbnail>
+                  <Article.Thumbnail>
+                    {ARTICLE_THUMBNAIL_IMG}
+                  </Article.Thumbnail>
                 </div>
               </Article>
             )
         }`,
     },
     {
-      code: `import {Article as AliasArticle} from 'tistory-react/theme'
+      code: `import {Article as AliasArticle} from 'tistory-react/theme/Article'
           const Components = () => {
             return (
               <AliasArticle>
                 <div>
-                <AliasArticle.Thumbnail>
-                {ARTICLE_THUMBNAIL_IMG}
-                </AliasArticle.Thumbnail>
+                  <AliasArticle.Thumbnail>
+                    {ARTICLE_THUMBNAIL_IMG}
+                  </AliasArticle.Thumbnail>
                 </div>
               </AliasArticle>
+            )
+        }`,
+    },
+    {
+      code: `import {Article, ARTICLE_COMMENT_COUNT} from 'tistory-react/theme/Article'
+      import {Comment} from 'tistory-react/theme/Comment'
+          const Components = () => {
+            return (
+              <Article>
+                <div>
+                  <Article.Thumbnail>
+                    <Article.ThumbnailImg />
+                  </Article.Thumbnail>
+                </div>
+              </Article>
+            )
+        }`,
+    },
+    {
+      code: `import {Article as AliasArticle} from 'tistory-react/theme/Article'
+          const Components = () => {
+            return (
+              <AliasArticle>
+                <div>
+                  <AliasArticle.Thumbnail>
+                    <AliasArticle.ThumbnailImg />
+                  </AliasArticle.Thumbnail>
+                </div>
+              </AliasArticle>
+            )
+        }`,
+    },
+    {
+      code: `import {Article, ARTICLE_COMMENT_COUNT} from 'tistory-react/theme/Article'
+      import {Comment} from 'tistory-react/theme/Comment'
+          const Components = () => {
+            return (
+              <>
+                <Article>
+                  <div>
+                    <Article.Thumbnail>
+                      <Article.ThumbnailImg />
+                      {ARTICLE_THUMBNAIL_IMG}
+                    </Article.Thumbnail>
+                    <Article.CommentCount>
+                      {ARTICLE_COMMENT_COUNT}
+                    </Article.CommentCount>
+                  </div>
+                </Article>
+                <Comment>
+                  <Comment.ListWrapper>
+                    <Comment.List />
+                  </Comment.ListWrapper>
+                </Comment>
+              </>
             )
         }`,
     },
   ],
   invalid: [
     {
-      code: `import {Article} from 'tistory-react/theme'
+      code: `import {Article, ARTICLE_COMMENT_COUNT} from 'tistory-react/theme/Article'
+      import {Comment} from 'tistory-react/theme/Comment'
           const Components = () => {
             return (
-              <Article.Thumbnail>
-                <Article>
-                {ARTICLE_THUMBNAIL_IMG}
-                </Article>
-              </Article.Thumbnail>
+              <div>
+                <Article.Thumbnail>
+                  {ARTICLE_THUMBNAIL_IMG}
+                </Article.Thumbnail>
+              </div>
             )
         }`,
       errors: 1,
     },
     {
-      code: `import {Article as AliasArticle} from 'tistory-react/theme'
+      code: `import {Article as AliasArticle} from 'tistory-react/theme/Article'
           const Components = () => {
             return (
-              <AliasArticle.Thumbnail>
-                <div>
-                  <AliasArticle>
+              <div>
+                <AliasArticle.Thumbnail>
                   {ARTICLE_THUMBNAIL_IMG}
-                  </AliasArticle>
+                </AliasArticle.Thumbnail>
+              </div>
+            )
+        }`,
+      errors: 1,
+    },
+    {
+      code: `import {Article, ARTICLE_COMMENT_COUNT} from 'tistory-react/theme/Article'
+      import {Comment} from 'tistory-react/theme/Comment'
+          const Components = () => {
+            return (
+              <Article>
+                <div>
+                  <Article.ThumbnailImg />
                 </div>
-              </AliasArticle.Thumbnail>
+              </Article>
+            )
+        }`,
+      errors: 1,
+    },
+    {
+      code: `import {Article as AliasArticle} from 'tistory-react/theme/Article'
+          const Components = () => {
+            return (
+              <AliasArticle>
+                <div>
+                  <AliasArticle.ThumbnailImg />
+                </div>
+              </AliasArticle>
+            )
+        }`,
+      errors: 1,
+    },
+    {
+      code: `import {Article, ARTICLE_COMMENT_COUNT} from 'tistory-react/theme/Article'
+      import {Comment} from 'tistory-react/theme/Comment'
+          const Components = () => {
+            return (
+              <>
+                <Article>
+                  <div>
+                    <Article.Thumbnail>
+                      <Article.ThumbnailImg />
+                    </Article.Thumbnail>
+                    <Article.CommentCount>
+                      {ARTICLE_COMMENT_COUNT}
+                    </Article.CommentCount>
+                  </div>
+                </Article>
+                <Comment>
+                  <Comment.List />
+                </Comment>
+              </>
             )
         }`,
       errors: 1,
